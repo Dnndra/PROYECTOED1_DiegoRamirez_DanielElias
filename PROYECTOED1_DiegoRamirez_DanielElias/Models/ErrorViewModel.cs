939bc600ace986a4894e47b3f275fba1ddaf1981
Dandres2001 <dandres21nw@gmail.com>
2021-04-04 12:10:13 -0600

    Agregar archivos de proyecto.

---
+++ PROYECTOED1_DiegoRamirez_DanielElias/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PROYECTOED1_DiegoRamirez_DanielElias.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PROYECTOED1_DiegoRamirez_DanielElias/Models/Paciente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PROYECTOED1_DiegoRamirez_DanielElias.Models
+{
+    public class Paciente : IComparable
+
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Required]
+        public string DPI { get; set; }
+
+        [Required]
+        public string Departamento { get; set; }
+
+        [Required]
+        public string Municipio { get; set; }
+        [Required]
+        public int OrderedQuantity { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var ordertree = ((Paciente)obj).Nombre;
+            return ordertree.CompareTo(Nombre);
+        }
+    }
+}
